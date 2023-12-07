2867410135d89d2b2a8651e495f938d873553556
Chamomile <dltldms5271@gmail.com>
2023-12-07 15:43:27 +0900

    3

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 //
 // 2023.12.07 
 // Week 14
-// Practice 2
+// Practice 3
 // 1914022 _ Sieun LEE
 //
  
@@ -9,29 +9,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Book 
-{
+struct Book{
        int number;
        char title[10];
 };
 
-int main(int argc, char *argv[])
-{
-    struct Book *p;
-    
-    p = (struct Book*)malloc( 2*sizeof(struct Book*));
-    
-    p->number = 1;
-    strcpy(p->title, "C Programming");
-    
-    (p+1)->number = 2;
-    strcpy((p+1)->title, "Electronics");
-    
-    free(p);
-    
-    system("PAUSE");
-    return 0;
-}
-
-
+void main(void){
+     int i;
+     struct Book **bookshelf;
+     
+     bookshelf = (struct Book**)malloc(sizeof(struct Book*)); // 구조체 포인터 변수 3개 할당 
+     for(i=0; i<3; i++)
+         bookshelf[i] = malloc(10*sizeof(struct Book)); //구조체 10개 할당 
+         
+     bookshelf[1][3].number = 5;
+     strcpy(bookshelf[1][3].title, "C++ Programming");
+     
+     (bookshelf[2]+4)-> number = 3;
+     strcpy((bookshelf[2]+4) -> title, "Communications Theroy");
+     
+     printf("book(1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
+     printf("book(2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
+     
+     //구조체 포인터 3개가 각각 가리키는 주소 해제
+     for (i=0; i<3; i++)
+         free(bookshelf[i]);
+     //구조체 이중 포인터가 가리키는 주소 해제
+     free(bookshelf);
+     
+     system("PAUSE");
+     
+     } 
+         
 
